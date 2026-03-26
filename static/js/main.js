75b4190ed8d1c1252785257bc2a14180e1eb027b
MehediMK <mehedikhan.cse@gmail.com>
2026-03-27 05:38:52 +0600

    [Add] Implement to-let project with tailwind

--- static/js/main.js
+++
@@ -1,5 +0,0 @@
-// Custom JavaScript for Rent&Stay - Add any custom scripts here
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Rent&Stay website loaded successfully');
-});
